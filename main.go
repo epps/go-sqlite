198cfb84085d46e64ebd6ef59aec2fd0f27d499c
Erik Eppel <erik@featureform.com>
2023-02-19 06:38:05 -0800

    completes part 1: intro and repl

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const (
+	EXIT_SUCCESS = iota
+	EXIT_FAILURE
+)
 
 func main() {
-	fmt.Println("SQLite clone to come ...")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("db > ")
+		command := readInput(reader)
+
+		if command == ".exit" {
+			os.Exit(EXIT_SUCCESS)
+		} else {
+			fmt.Printf("Unrecognized command %s.\n", command)
+		}
+	}
+}
+
+func readInput(reader *bufio.Reader) string {
+	command, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	command = strings.TrimRight(command, "\n")
+	if len(command) == 0 {
+		fmt.Println("Error reading input")
+		os.Exit(EXIT_FAILURE)
+	}
+
+	return command
 }
