bdb0022ff4fd8fe8db390fc4c8a07961355a7c9d
Erik Eppel <erik@featureform.com>
2023-02-19 08:23:56 -0800

    completes part 2: simple sql compiler

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## Overview
 
-Working from _[Let's Build a Simple Database](https://cstack.github.io/db_tutorial/)_ tutorial to better learn Go and understand database internals.
+I'll be working through _[Let's Build a Simple Database](https://cstack.github.io/db_tutorial/)_ tutorial to understand database internals while learning Go at the same time.
+
+I've found that walking through "build your own X" tutorials cross-language (e.g. the tutorial examples are in Java, but I implement X in Rust) forces me to genuinely think about what I'm trying to do.
--- main.go
+++ main.go
@@ -7,9 +7,22 @@ import (
 	"strings"
 )
 
+type MetaCommandResult int
+type PrepareResult int
+type StatementType int
+type Statement struct {
+	sType StatementType
+}
+
 const (
-	EXIT_SUCCESS = iota
-	EXIT_FAILURE
+	EXIT_SUCCESS                                        = 0
+	EXIT_FAILURE                                        = 1
+	META_COMMAND_SUCCESS              MetaCommandResult = 0
+	META_COMMAND_UNRECOGNIZED_COMMAND MetaCommandResult = 1
+	PREPARE_SUCCESS                   PrepareResult     = 0
+	PREPARE_UNRECOGNIZED_STATEMENT    PrepareResult     = 1
+	STATEMENT_INSERT                  StatementType     = 0
+	STATEMENT_SELECT                  StatementType     = 1
 )
 
 func main() {
@@ -19,11 +32,26 @@ func main() {
 		fmt.Print("db > ")
 		command := readInput(reader)
 
-		if command == ".exit" {
-			os.Exit(EXIT_SUCCESS)
-		} else {
-			fmt.Printf("Unrecognized command %s.\n", command)
+		if strings.HasPrefix(command, ".") {
+			switch doMetaCommand(command) {
+			case META_COMMAND_SUCCESS:
+				continue
+			case META_COMMAND_UNRECOGNIZED_COMMAND:
+				fmt.Printf("Unrecognized command %s.\n", command)
+				continue
+			}
 		}
+
+		stm := &Statement{}
+		switch prepareStatement(command, stm) {
+		case PREPARE_SUCCESS:
+		case PREPARE_UNRECOGNIZED_STATEMENT:
+			fmt.Printf("Unrecognized keyword at start of '%s'.\n", command)
+			continue
+		}
+
+		executeStatement(stm)
+		fmt.Println("Executed.")
 	}
 }
 
@@ -39,4 +67,34 @@ func readInput(reader *bufio.Reader) string {
 	}
 
 	return command
+}
+
+func doMetaCommand(command string) MetaCommandResult {
+	if command == ".exit" {
+		os.Exit(EXIT_SUCCESS)
+	}
+	return META_COMMAND_UNRECOGNIZED_COMMAND
+
+}
+
+func prepareStatement(command string, statement *Statement) PrepareResult {
+	if strings.HasPrefix(command, "select") {
+		statement.sType = STATEMENT_SELECT
+		return PREPARE_SUCCESS
+	}
+	if strings.HasPrefix(command, "insert") {
+		statement.sType = STATEMENT_INSERT
+		return PREPARE_SUCCESS
+	}
+
+	return PREPARE_UNRECOGNIZED_STATEMENT
+}
+
+func executeStatement(stm *Statement) {
+	switch stm.sType {
+	case STATEMENT_INSERT:
+		fmt.Println("This is where we would do an insert")
+	case STATEMENT_SELECT:
+		fmt.Println("This is where we would do an select")
+	}
 }
